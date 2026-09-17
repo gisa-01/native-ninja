f3da111e1a2c6d178341de66449d5cc99c4782ba
Gisa <gisaaimesavio@gmail.com>
2026-09-17 16:56:43 +0200

    Project setup

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <Text>index</Text>
+    </SafeAreaView>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
