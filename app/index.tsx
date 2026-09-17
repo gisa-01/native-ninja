4c7ed7cdb2708934426907b8000760a966e44221
Gisa <gisaaimesavio@gmail.com>
2026-09-17 21:24:43 +0200

    Text, View, and Image components

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Logo from '../assets/img/logo_light.jpg'
 
 const index = () => {
   return (
-    <SafeAreaView>
-      <Text>index</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.image}/>
+      <Text style={styles.title}>Number 1</Text>
+      <Text style={{marginTop:10, marginBottom: 30}}>
+        Reading List App
+      </Text>
+    </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center', 
+    justifyContent:'center'
+  },
+  title:{
+    fontWeight:'bold',
+    fontSize:18
+  },
+  image: {
+    marginVertical: 20,
+    height:100,
+    width:100
+  },
+
+})
