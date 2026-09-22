e7384d784c8830db1ab40570cccb281a2405e1d2
Gisa <gisaaimesavio@gmail.com>
2026-09-22 23:55:07 +0200

    Starting the auth context

---
+++ contexts/UserContext.tsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+
+export const UserContext = createContext()
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState();
+
+  async function  login (email,password) {
+
+  }
+
+  async function  register (email,password) {
+
+  }
+
+  async function  logout () {
+
+  }
+  return(
+    <UserContext.Provider value=({user, login, register, logout})>
+
+    </UserContext.Provider>
+  )
+}
+
